cd76b689b7829bd06fb84d66c9982cd623ef537a
Primarily Snark <starkey.joshua@gmail.com>
2016-05-07 16:44:45 -0700

    Add initial build tooling.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry:  './src',
+  output: {
+    path:     'dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(jpg|jpeg)$/,
+        loader: 'url-loader',
+        query: {
+          limit: 100000
+        },
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!less-loader'),
+      },
+      {
+        test:   /\.jsx?$/,
+        excluse: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        },
+      },
+      {
+        test: require.resolve("react"),
+        loader: "expose?React"
+      }
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css', {
+      allChunks: true,
+    }),
+  ],
+};
