99b3b40874576914be8e94d60121f6faff537d37
Primarily Snark <starkey.joshua@gmail.com>
2016-05-15 16:44:06 -0700

    Replace hashHistory with browserHistory.

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import { CharacterCreator, CharacterCreatorOverview, CharacterList, CharacterHeader } from './components/characters';
 import { Component } from 'react';
-import { IndexRoute, Route, Router, hashHistory } from 'react-router';
+import { IndexRoute, Route, Router, browserHistory } from 'react-router';
 import App from './components/app';
 
 export default class Routes extends Component {
   render() {
     return (
-      <Router history={hashHistory}>
+      <Router history={browserHistory}>
         <Route path="/" component={App}>
           <Route path="characters" component={CharacterHeader}>
             <IndexRoute component={CharacterList} />
